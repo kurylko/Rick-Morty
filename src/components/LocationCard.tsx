05fcf87af4fb86ae83483134977a25dcfdbb713c
kurylko <reviliya@gmail.com>
2023-03-28 01:23:28 +0200

    basic css for locations page

---
+++ src/components/LocationCard.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import {ICharacter, ILocation} from "../types/interfaces";
+
+const LocationCard = (props: ILocation) => {
+    const {name, type, dimension, number_of_residents} = props;
+
+    return (
+        <div className="locationCard">
+            <div className="location-name">{name}</div>
+            <div className="location-infoBox">
+                <div className="location-question">
+                    <li className="location-info">Type:</li>
+                    <li className="location-info">Dimension:</li>
+                    <li className="location-info">Number of residents:</li>
+                </div>
+                <div className="location-answer">
+                    <li className="location-info">{type}</li>
+                    <li className="location-info">{dimension}</li>
+                    <li className="location-info">{number_of_residents}</li>
+                </div>
+            </div>
+
+        </div>
+    )
+};
+
+export default LocationCard
--- src/pages/Locations.tsx
+++ src/pages/Locations.tsx
@@ -1,7 +1,40 @@
+import {useEffect, useState} from "react";
+import axios from "axios";
+import LocationCard from "../components/LocationCard";
+import {ILocation} from "../types/interfaces";
+
 const Locations = () => {
+    const [locations, setLocations] = useState([]);
+
+    useEffect(() => {
+        axios.get('https://rickandmortyapi.com/api/location')
+            .then(response => {
+                setLocations(response.data.results)
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }, []);
+
+    const locationsList = locations;
+    console.log(locationsList)
+
+    let numberOfResidents = locationsList.forEach((location) =>
+        location.residents.length;
+)
+
+    console.log(numberOfResidents)
+
     return (
-        <div>
+        <div className="locations-page">
             Locations page
+            {locationsList.map((location: ILocation) => (
+                <LocationCard id={location.id}
+                              name={location.name}
+                              type={location.type}
+                              dimension={location.dimension}
+                              residents={location.residents}/>
+            ))}
         </div>
     )
 }
