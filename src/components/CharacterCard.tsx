2b5a763d2664e4549ff8b11ed0cf5046c81448c1
kurylko <reviliya@gmail.com>
2024-09-16 21:29:49 +0200

    Link from router

--- src/components/CharacterCard.tsx
+++ src/components/CharacterCard.tsx
@@ -21,33 +21,6 @@ const CharacterCard = ({image, name, status, species, gender, origin, location} 
             </div>
         </div>
     )
-
-    {/*
-        <div className="characterCard">
-            <div className="character-image-container">
-                <img className="character-image" src={image} alt={`${name} image`}/>
-            </div>
-            <div className="character-infobox">
-                <div className="character-name">{name} </div>
-                <div className="characters-infobox-info">
-                    <div className="character-info-questions">
-                        <li className="character-info">Status:</li>
-                        <li className="character-info">Species:</li>
-                        <li className="character-info">Gender:</li>
-                        <li className="character-info">Origin:</li>
-                        <li className="character-info">Location:</li>
-                    </div>
-                    <div className="character-info-answers">
-                        <li className="character-info">{status}</li>
-                        <li className="character-info">{species}</li>
-                        <li className="character-info">{gender}</li>
-                        <li className="character-info">{origin.name}</li>
-                        <li className="character-info">{location.name}</li>
-                    </div>
-                </div>
-            </div>
-        </div>
-        */}
 }
 
 export default CharacterCard
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,20 +1,21 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 const Menu = () => {
 
     return (
         <div className="flex w-screen h-16 bg-primary text-white font-normal sticky top-0 delay-1000 justify-between">
             <div className="flex justify-center items-center w-full gap-4 list-none pb-1">
-                <a href="characters" className="link link-hover">
+                <Link to="/characters" className="link link-hover">
                     <button
                         className="btn btn-outline text-white p-2.5 border-lg border-solid border-1 border-white hover:bg-border">Characters
                     </button>
-                </a>
-                <a href="locations" className="link link-hover">
+                </Link>
+                <Link to="/locations" className="link link-hover">
                     <button
                         className="btn btn-outline text-white p-2.5 border-lg border-solid border-1 border-white hover:bg-border">Locations
                     </button>
-                </a>
+                </Link>
             </div>
         </div>
     )
